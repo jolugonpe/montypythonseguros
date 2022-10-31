7afba0c900bddcd2079d227f37689c554ad729a2
jolugonpe <jolugonpe@gmail.com>
2022-10-31 20:21:48 -0300

    validación de mail

--- script.js
+++ script.js
@@ -52,7 +52,7 @@
 })();
 
 
-//Seguros
+//Seguros - Desplegado
 (function(){
     const titleSeguros = [...document.querySelectorAll('.seguros__title')];
     console.log(titleSeguros)
@@ -76,3 +76,13 @@
 })();
 
 
+//Validación de Formulario
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("¡Se esperaba una dirección de correo electrónico!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
